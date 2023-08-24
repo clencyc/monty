2a277882bdb18680def77c560a5d2692619f9707
festus-sulumeti <sulumetifestus@outlook.com>
2023-08-24 02:38:39 -0700

    nopp

---
+++ nopp.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+* noper_in - The function that is going to be used to do nothing
+* @stack: pointer to top node of stack
+* @lline_n: The line number that is going to be used
+*/
+void noper_in(stack_t **stack, unsigned int lline_n)
+{
+(void)stack;
+(void)lline_n;
+}
